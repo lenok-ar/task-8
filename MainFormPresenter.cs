ea272bfd2fd64f75ebedb26f869df20e804f4d56
lenok-ar <leno4ka279a@gmail.com>
2025-04-29 22:59:16 +0700

    init presenter

---
+++ MainFormPresenter.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DirectorySync
+{
+  public class MainFormPresenter : IMainFormPresenter
+  {
+    private IMainFormView view;
+    private readonly ISyncService syncService;
+    private CancellationTokenSource cts;
+
+    public MainFormPresenter(IMainFormView view, ISyncService syncService)
+    {
+      this.syncService = syncService;
+      this.view = view;
+    }
+
+    public void SetView(IMainFormView view)
+    {
+      this.view = view;
+      this.view.BrowseSourceClicked += View_BrowseSourceClicked;
+      this.view.BrowseDestinationClicked += View_BrowseDestinationClicked;
+      this.view.SyncButtonClicked += View_SyncButtonClicked;
+    }
+
+    private void View_BrowseSourceClicked(object sender, EventArgs e)
+    {
+      using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+      {
+        if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+        {
+          view.SourceDirectory = folderBrowserDialog.SelectedPath;
+        }
+      }
+    }
+
+    private void View_BrowseDestinationClicked(object sender, EventArgs e)
+    {
+      using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+      {
+        if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+        {
+          view.DestinationDirectory = folderBrowserDialog.SelectedPath;
+        }
+      }
+    }
+
+    private async void View_SyncButtonClicked(object sender, EventArgs e)
+    {
+      if (string.IsNullOrEmpty(view.SourceDirectory) || string.IsNullOrEmpty(view.DestinationDirectory))
+      {
+        view.ShowMessage("Пожалуйста, выберите как исходный, так и конечный каталоги.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        return;
+      }
+
+      if (view.IsSyncing)
+      {
+        cts.Cancel();
+        return;
+      }
+
+      view.IsSyncing = true;
+      view.SyncButtonText = "Отмена";
+      cts = new CancellationTokenSource();
+
+      try
+      {
+        view.ClearLog();
+        var progress = new Progress<string>(message => view.AppendLog(message));
+
+        await syncService.SynchronizeDirectories(view.SourceDirectory, view.DestinationDirectory, cts.Token, progress);
+        await syncService.SynchronizeDirectories(view.DestinationDirectory, view.SourceDirectory, cts.Token, progress);
+
+        view.ShowMessage("Синхронизация завершена.", "Успешно!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+      }
+
+      catch (OperationCanceledException)
+      {
+        view.ShowMessage("Синхронизация отменена.", "Отменена.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+      }
+
+      catch (Exception ex)
+      {
+        view.ShowMessage($"Сбой синхронизации: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+      }
+
+      finally
+      {
+        view.IsSyncing = false;
+        view.SyncButtonText = "Синхронизировать";
+      }
+    }
+
+    public void OnBrowseSourceClicked()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void OnBrowseDestinationClicked()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void OnSyncButtonClicked()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
